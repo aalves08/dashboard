a438d9216da74f4c67142d4c21671d2278c4e163
Alexandre Alves <alexandre.alves@suse.com>
2024-11-11 15:57:11 +0000

    testing exports

--- shell/scripts/retrieve-versions.sh
+++ shell/scripts/retrieve-versions.sh
@@ -10,34 +10,21 @@ BASE_DIR="$(
 SHELL_DIR=$BASE_DIR/shell
 CREATORS_DIR=$BASE_DIR/creators/extension
 
-if [ -z "$BRANCH" ]; then
-  echo "You need to specify a BRANCH variable first!"
-  exit 1
-fi
-
 function generate_version_export() {
   NAME=$1
 
   case $NAME in
     "shell")
-      SHELL_VERSION=$(jq -r .version ${SHELL_DIR}/package.json)
-      declare SHELL_VERSION_$BRANCH=${SHELL_VERSION}
-      varname=SHELL_VERSION_$BRANCH
-      echo shell version ::: ${!varname}
+      SHELL_V=$(jq -r .version ${SHELL_DIR}/package.json)
+      echo "Shell version retrieved ::: ${SHELL_V}"
+      export CREATORS_VERSION=SHELL_V
       ;;
     "creators")
-      CREATORS_VERSION=$(jq -r .version ${CREATORS_DIR}/package.json)
-      declare CREATORS_VERSION_$BRANCH=${CREATORS_VERSION}
-      varname=CREATORS_VERSION_$BRANCH
-      echo ${!varname}
-      echo rancher version ::: ${!varname}
+      CREATORS_V=$(jq -r .version ${CREATORS_DIR}/package.json)
+      echo "Creators version retrieved ::: ${CREATORS_V}"
       ;;
   esac
 }
 
-echo "Retrieve current Shell version"
-
 generate_version_export "shell"
 generate_version_export "creators"
-
-# export RANCHER_VERSION_E2E=${{ matrix.rancherEnv[1] }}#
