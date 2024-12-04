011e558597072aba35063ba37a546110e8943586
Alexandre Alves <alexandre.alves@suse.com>
2024-12-04 15:41:35 +0000

    update

--- .github/workflows/scripts/test-extensions-creators-build.sh
+++ .github/workflows/scripts/test-extensions-creators-build.sh
@@ -2,19 +2,14 @@
 
 set -eo pipefail
 
-EXTERNAL_TAG=$1
-EXTERNAL_UPDATE=$2
-
-if [ -z "${EXTERNAL_TAG}" ]; then
-  echo "Missing tag set on running this script! Cannot run without a tag!"
-  exit 1
-fi
 
 validate_tagged_extension_creator() {
   TAG=$1
+  NODE_VERSION=$2
+  UPDATE=$3
 
-  if [ -n "$2" ]; then
-    echo "will also cover the update path and migration on this run"
+  if [ -n "$UPDATE" ]; then
+    echo "*** Will also cover the UPDATE path and MIGRATION on this run! ***"
     UPDATE="true"
   fi
 
@@ -23,6 +18,11 @@ validate_tagged_extension_creator() {
 
   echo "*** Verifying extension creator for tag ::: ${TAG} ***"
   echo "Using temporary directory ${DIR}"
+
+  echo "=> Setting up node version required for this env: ${NODE_VERSION}"
+
+  nvm install ${NODE_VERSION}
+  nvm use ${NODE_VERSION}
 
   # TODO: change yarn create to use the new tag approach (jordon to tweak code upstream)
   npm init @rancher/extension@${TAG} test-pkg --app-name test-app | cat
@@ -35,10 +35,10 @@ validate_tagged_extension_creator() {
 
 
   if [ $UPDATE == "true" ]; then
-    echo "*** Testing full update path for extensions ***"
-    echo "Testing update from legacy-v1 to legacy-v2"
+    echo "*** Testing FULL UPGRADE path for extensions ***"
+    echo "Testing UPGRADE from legacy-v1 to legacy-v2"
 
-    npm init @rancher/extension@legacy-v2 -- --update
+    npm init @rancher/extension@legacy-v2 -- --update -f
 
     rm -rf node_modules
     rm -rf yarn.lock
@@ -47,7 +47,12 @@ validate_tagged_extension_creator() {
 
     yarn build-pkg test-pkg | cat
 
-    echo "Testing update from legacy-v2 to latest"
+    echo "*** Testing UPGRADE from legacy-v2 to latest ***"
+
+    echo "=> Updating node version required for last leg of upgrade path: v20"
+
+    nvm install v20
+    nvm use v20
 
     npm init @rancher/extension -- --migrate
 
@@ -67,9 +72,9 @@ validate_tagged_extension_creator() {
 }
 
 # test creating an extension with latest shell releases + build
-validate_tagged_extension_creator ${EXTERNAL_TAG} ${EXTERNAL_UPDATE}
-# validate_tagged_extension_creator "legacy-v2"
-# validate_tagged_extension_creator "latest"
+validate_tagged_extension_creator "legacy-v1" "v16"
+validate_tagged_extension_creator "legacy-v2" "v16"
+validate_tagged_extension_creator "latest" "v20"
 
 # test update paths + build
-# validate_tagged_extension_creator "legacy-v1" "true"
+validate_tagged_extension_creator "legacy-v1"  "v16" "true"
