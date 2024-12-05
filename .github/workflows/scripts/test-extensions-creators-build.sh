2bacac89ab8bbe3db5048a9d7a5bc591fe55fe4c
Alexandre Alves <alexandre.alves@suse.com>
2024-12-05 10:30:32 +0000

    convert back to bash

--- .github/workflows/scripts/test-extensions-creators-build.sh
+++ .github/workflows/scripts/test-extensions-creators-build.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-set -e
+set -eo pipefail
 
 SKELETON_APP_NAME="test-pkg"
+
 
 validate_tagged_extension_creator() {
   TAG=$1
@@ -14,13 +15,12 @@ validate_tagged_extension_creator() {
     UPDATE="true"
   fi
 
-  # These two commands will allow for NVM to work in sh
-  export NVM_DIR="$HOME/.nvm"
-  # shellcheck disable=SC1090
-  . "$NVM_DIR/nvm.sh"
+  # these two commands will allow for NVM to work in bash, since it's included in ubuntu-latest
+  export NVM_DIR=~/.nvm
+  source ~/.nvm/nvm.sh
 
   DIR=$(mktemp -d)
-  cd "$DIR" || exit 1
+  cd $DIR
 
   echo "*** ***************************************** ***"
   echo "*** Verifying extension creator for tag ::: ${TAG} ***"
@@ -31,24 +31,24 @@ validate_tagged_extension_creator() {
   echo "=> Current dir 1:"
   pwd
 
-  # Setting up the correct version of Node  
-  nvm install "${NODE_VERSION}"
-  nvm use "${NODE_VERSION}"
+  # setting up correct version of node  
+  nvm install ${NODE_VERSION}
+  nvm use ${NODE_VERSION}
 
-  # Generate skeleton app
-  npm init @rancher/extension@"${TAG}" "${SKELETON_APP_NAME}" --app-name test-app | cat
-  cd "${SKELETON_APP_NAME}" || exit 1
+  # generate skeleton app
+  npm init @rancher/extension@${TAG} ${SKELETON_APP_NAME} --app-name test-app | cat
+  cd ${SKELETON_APP_NAME}
 
-  # Install dependencies
+  # install dependencies
   yarn install
 
-  # Test build of pkg inside skeleton app
-  yarn build-pkg "${SKELETON_APP_NAME}" | cat
+  # test build of pkg inside skeleton app
+  yarn build-pkg ${SKELETON_APP_NAME} | cat
 
   echo "=> Current dir 2:"
   pwd
 
-  if [ "$UPDATE" = "true" ]; then
+  if [ $UPDATE == "true" ]; then
     echo "*** ***************************************** ***"
     echo "*** Testing FULL UPGRADE path for extensions ***"
     echo "*** ***************************************** ***"
@@ -58,9 +58,10 @@ validate_tagged_extension_creator() {
     pwd
 
     git init
-    # When doing git init, we are sent to the .git folder
+    #when doing git init, we are sent to .git folder
     cd ..
-    cd "${SKELETON_APP_NAME}" || exit 1
+    cd ${SKELETON_APP_NAME}
+    cd pkg
 
     echo "=> Current dir 4:"
     pwd
@@ -68,11 +69,11 @@ validate_tagged_extension_creator() {
     npm init @rancher/extension@legacy-v2 -- --update
 
     rm -rf node_modules
-    rm -f yarn.lock
+    rm -rf yarn.lock
 
     yarn install
 
-    yarn build-pkg "${SKELETON_APP_NAME}" | cat
+    yarn build-pkg ${SKELETON_APP_NAME} | cat
 
     echo "*** ***************************************** ***"
     echo "*** Testing UPGRADE from legacy-v2 to latest ***"
@@ -86,135 +87,24 @@ validate_tagged_extension_creator() {
     npm init @rancher/extension -- --migrate
 
     rm -rf node_modules
-    rm -f yarn.lock
+    rm -rf yarn.lock
 
     yarn install
 
-    yarn build-pkg "${SKELETON_APP_NAME}" | cat
+    yarn build-pkg ${SKELETON_APP_NAME} | cat
   fi
 
   echo "Cleaning temporary dir"
-  cd "${DIR}" || exit 1
+  cd ${DIR}
 
   echo "Removing folder ${DIR}"
-  rm -rf "${DIR}"
+  rm -rf ${DIR}
 }
 
-# Test creating an extension with latest shell releases + build
+# test creating an extension with latest shell releases + build
 # validate_tagged_extension_creator "legacy-v1" "v16"
 # validate_tagged_extension_creator "legacy-v2" "v16"
 # validate_tagged_extension_creator "latest" "v20"
 
-# Test update paths + build
+# test update paths + build
 validate_tagged_extension_creator "legacy-v1"  "v16" "true"
-
-
-# #!/usr/bin/env bash
-
-# set -eo pipefail
-
-# SKELETON_APP_NAME="test-pkg"
-
-
-# validate_tagged_extension_creator() {
-#   TAG=$1
-#   NODE_VERSION=$2
-#   UPDATE=$3
-
-#   if [ -n "$UPDATE" ]; then
-#     echo "*** Will also cover the UPDATE path and MIGRATION on this run! ***"
-#     UPDATE="true"
-#   fi
-
-#   # these two commands will allow for NVM to work in bash, since it's included in ubuntu-latest
-#   export NVM_DIR=~/.nvm
-#   source ~/.nvm/nvm.sh
-
-#   DIR=$(mktemp -d)
-#   cd $DIR
-
-#   echo "*** ***************************************** ***"
-#   echo "*** Verifying extension creator for tag ::: ${TAG} ***"
-#   echo "*** ***************************************** ***"
-#   echo "Using temporary directory ${DIR}"
-
-#   echo "=> Setting up node version required for this env: ${NODE_VERSION}"
-#   echo "=> Current dir 1:"
-#   pwd
-
-#   # setting up correct version of node  
-#   nvm install ${NODE_VERSION}
-#   nvm use ${NODE_VERSION}
-
-#   # generate skeleton app
-#   npm init @rancher/extension@${TAG} ${SKELETON_APP_NAME} --app-name test-app | cat
-#   cd ${SKELETON_APP_NAME}
-
-#   # install dependencies
-#   yarn install
-
-#   # test build of pkg inside skeleton app
-#   yarn build-pkg ${SKELETON_APP_NAME} | cat
-
-#   echo "=> Current dir 2:"
-#   pwd
-
-#   if [ $UPDATE == "true" ]; then
-#     echo "*** ***************************************** ***"
-#     echo "*** Testing FULL UPGRADE path for extensions ***"
-#     echo "*** ***************************************** ***"
-#     echo "=> Testing UPGRADE from legacy-v1 to legacy-v2"
-
-#     echo "=> Current dir 3:"
-#     pwd
-
-#     git init
-#     #when doing git init, we are sent to .git folder
-#     cd ..
-#     cd ${SKELETON_APP_NAME}
-
-#     echo "=> Current dir 4:"
-#     pwd
-
-#     npm init @rancher/extension@legacy-v2 -- --update
-
-#     rm -rf node_modules
-#     rm -rf yarn.lock
-
-#     yarn install
-
-#     yarn build-pkg ${SKELETON_APP_NAME} | cat
-
-#     echo "*** ***************************************** ***"
-#     echo "*** Testing UPGRADE from legacy-v2 to latest ***"
-#     echo "*** ***************************************** ***"
-
-#     echo "=> Updating node version required for last leg of upgrade path: v20"
-
-#     nvm install v20
-#     nvm use v20
-
-#     npm init @rancher/extension -- --migrate
-
-#     rm -rf node_modules
-#     rm -rf yarn.lock
-
-#     yarn install
-
-#     yarn build-pkg ${SKELETON_APP_NAME} | cat
-#   fi
-
-#   echo "Cleaning temporary dir"
-#   cd ${DIR}
-
-#   echo "Removing folder ${DIR}"
-#   rm -rf ${DIR}
-# }
-
-# # test creating an extension with latest shell releases + build
-# # validate_tagged_extension_creator "legacy-v1" "v16"
-# # validate_tagged_extension_creator "legacy-v2" "v16"
-# # validate_tagged_extension_creator "latest" "v20"
-
-# # test update paths + build
-# validate_tagged_extension_creator "legacy-v1"  "v16" "true"
